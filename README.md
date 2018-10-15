887ebf01e34c6fd60dc751d85ba28092c2a5fd92
Bryan Weber <bryan.weber@looker.com>
2018-10-15 07:34:27 -0700

    added no change logic

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # info_scheming_web_app
+
+sudo apt-get update
+sudo apt-get install git
+
+# following guide here
+
+Setup Instance
+Open up HTTP and HTTPS in Security Group
+
+Create A Record in Route 53
+
+sudo touch /etc/nginx/sites-available/bryanfweber.com
+sudo chown -R $USER:$USER /etc/nginx/sites-available/bryanfweber.com
+
+vi /etc/nginx/sites-available/bryanfweber.com
+
+server {
+listen 80;
+server_name bryanfweber.com;
+location / {
+proxy_pass http://127.0.0.1:5000/;
+}
+}
+
+sudo ln -f -s /etc/nginx/sites-available/bryanfweber.com /etc/nginx/sites-enabled/bryanfweber.com
+sudo service nginx restart
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install python-certbot-nginx
+#use certbot nginx plugin for certificate installation
+sudo certbot --nginx 
+
+## install pip / files
+
+
+## github setup - https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+
+ssh-keygen -t rsa -b 4096 -C "bryan@looker.com"
+eval "$(ssh-agent -s)"
+ssh-add -k ~/.ssh/id_rsa
+
+### add id_rsa.pem to github repo
+
+pip install virtualenv
+
+virtualenv venv
+source ./venv/bin/activate
+
+
+
+pip install -r requirements.txt
+curl 
+git clone git@github.com:bryan-at-looker/info_scheming.git
---
+++ flask-info-scheming.py
@@ -0,0 +1,88 @@
+#!flask/bin/python
+import json
+import git
+from flask import Flask, request, abort
+from lookml import lookml, View, Dimension, DimensionGroup
+
+app = Flask(__name__)
+
+lookml.DB_FIELD_DELIMITER_START = ''
+lookml.DB_FIELD_DELIMITER_END = ''
+
+filename = 'generating_schema.view.lkml'
+git_repo = 'git@github.com:bryan-at-looker/info_scheming.git'
+git_dir = 'info_scheming'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+      print(request.json)
+      payload = request.json
+
+      data = payload['attachment']['data']
+
+      lookml = json.loads(data)[0]['info_scheming.lookml'];
+
+      g = git.cmd.Git(git_dir)
+      g.pull()
+      g.push()
+
+      f = open(git_dir+'/'+filename, 'w')
+      f.write(lookml)
+      f.close()
+      g.add(filename)
+      g.commit('-m', 'commit')
+      g.push()
+
+      return '', 200
+
+    else:
+        abort(400)
+        
+@app.route('/info_in_rows', methods=['POST'])
+def info_in_rows():
+    if request.method == 'POST':
+      print(request.json)
+      payload = request.json
+
+      data = json.loads(payload['attachment']['data'])
+      
+      vw = View(str(data[0]['info_in_rows.view_name_lookml']))
+      vw.setSqlTableName(sql_table_name=str(data[0]['info_in_rows.table_name']),schema=str(data[0]['info_in_rows.table_schema']))
+      vw.setFolder(git_dir)
+      
+      for row in data:
+        column = str(row['info_in_rows.column_name'])
+
+        if str(row['info_in_rows.type_convert']) == 'time':
+          dim = DimensionGroup(dbColumn=column)
+        else:
+          dim = Dimension(dbColumn=column)
+          dim.setType(str(row['info_in_rows.type_convert']))
+        
+        if row['info_in_rows.comment'] is not None:
+          dim.setProperty('description', str(row['info_in_rows.comment']))
+          
+        vw + dim
+
+      g = git.cmd.Git(git_dir)
+      g.pull()
+      vw.path = git_dir + '/' + vw.fileName # REMOVE WHEN write is updated
+      vw.write()
+      g.add(vw.fileName)
+      repo = git.Repo(git_dir)
+      changedFiles = repo.index.diff("HEAD")
+      if ( changedFiles ):
+        g.commit('-m', '"new commit"')
+        g.push()
+      else:
+        print "No files updated"
+
+      return '', 200
+
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", threaded=True, debug=True)
